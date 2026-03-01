93703b8ba5e845918e0d11d195b39bc2e232c87e
daniel-gerhard <daniel.gerhard@canterbury.ac.nz>
2026-03-02 11:44:12 +1300

    treat single random effect variance as matrix. Return named vector with estimate and std error

--- R/ed.R
+++ R/ed.R
@@ -1,14 +1,14 @@
 #' Effective dose estimation
 #'
 #' @param x a drmTMB object
-#' @param respLev effective dose level
+#' @param respLev effective dose level, default is 50
 #' @param random logical, if TRUE, the variance-covariance matrix of the random effects is included in the standard error estimation
 #' @param residual logical, if TRUE, the residual variance is included in the standard error estimation
 #'
 #' @return a vector with estimates
 #' @export
 #'
-ed <- function(x, respLev, random=TRUE, residual=FALSE){ 
+ed <- function(x, respLev=50, random=TRUE, residual=FALSE){ 
   p <- respLev/100
   est <- x$sdrl$par.fixed
   covest <- x$sdrl$cov.fixed
@@ -32,6 +32,7 @@ ed <- function(x, respLev, random=TRUE, residual=FALSE){
     cr <- matrix(corrs[,1], nrow=sqrt(nrow(corrs)))
     sdm <- exp(thetas[1:sqrt(nrow(corrs))])
     rname <- all.vars(x$rform[[2]])
+    if (length(sdm) == 1) sdm <- as.matrix(sdm)
     vc <- diag(sdm) %*% cr %*% diag(sdm)
     idr <- paste("b", 1:5, sep="") %in% rname
     rvc <- matrix(0, nrow=5, ncol=5)
@@ -98,5 +99,7 @@ ed <- function(x, respLev, random=TRUE, residual=FALSE){
     std <- sqrt(grad %*% cvc4 %*% t(grad))
   }
 
-  return(c(edl, std))
+  res <- c(edl, as.vector(std))
+  names(res) <- c("Estimate", "Std. Error")
+  return(res)
 }
